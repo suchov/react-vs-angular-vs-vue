4df27fff5c8214395f4d1e4831f87f411077c206
suchov <artem.suchov@gmail.com>
2018-04-09 18:34:40 +0300

    finish the Home page

--- vuejs/src/components/Home.vue
+++ vuejs/src/components/Home.vue
@@ -1,16 +1,98 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero">
+      <div class="hero-dody">
+        <div class="container">
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{ subheading }}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn" href="#">Learn more</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+          <div class="column is-1 has-text-centred">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+          <div class="column is-one-third-desktop">
+            <p class="title"><strong>We provide superior logistics so that your business can succeed in a crazy world.</strong></p>
+          </div>
+          <div class="column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incicicunt ut labore...</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this thing.</p>
+              <p class="subtitle">- Gray Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this thing.</p>
+              <p class="subtitle">- Gray Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this thing.</p>
+              <p class="subtitle">- Gray Simon</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      heading: 'Soaring to new heights',
+      subheading: 'Lorem ipsum dolor sit amet, counsecteur adipiscing elit, sed do eiusmod ...'
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import '../mq'
+
+.title
+  +mobile
+    font-weight: bold
+  +tablet
+    font-size: 2rem
+  +desktop
+    font-size: 4rem
+    margin-top: 2rem
+
+h2
+  margin: 1.5rem 0 2rem 0 !important
+
+.fa-cog
+  font-size: 40px
+
+#learn
+  +desktop
+    margin-bottom: 2rem
+
+.pd
+  +tablet
+    padding: 2em 0
 </style>
